8bcd5d2f30d6d4f1b80a06231a469149c803c36f
yanhao <yhzhu@umich.edu>
2019-08-11 22:15:32 -0400

    Create an empty workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/yanhao/Desktop/ROS/ROS_playgroud/src
+# Build directory: /home/yanhao/Desktop/ROS/ROS_playgroud/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yanhao/Desktop/ROS/ROS_playgroud/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/melodic/bin:/home/yanhao/.conda/envs/ros/bin:/home/yanhao/miniconda2/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PWD="/home/yanhao/Desktop/ROS/ROS_playgroud/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/yanhao/Desktop/ROS/ROS_playgroud/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/yanhao/Desktop/ROS/ROS_playgroud/src:$ROS_PACKAGE_PATH"
